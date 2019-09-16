80ce2e3c58c651f2831e4532fafbc0d573093d89
Khaled Pendleton <khaledpen@hotmail.com>
2019-09-16 15:01:58 -0400

    Set up dev and production webpack files. Configured webpack dev server

---
+++ webpack.common.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/template.html'
+    })]
+};
