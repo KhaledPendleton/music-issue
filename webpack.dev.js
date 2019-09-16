9b932763cbc0f550a9dea6e3b655d4d6836c01f0
Khaled Pendleton <khaledpen@hotmail.com>
2019-09-16 17:51:52 -0400

    Configured webpack for development and production

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,29 @@
 const path = require('path');
 const merge = require('webpack-merge');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'none',
     output: {
-        filename: 'app.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: './src/template.html'})
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
     }
 });
