85090cf5e8142fe85e83d1b6a29633ae620ca903
Khaled Pendleton <khaledpen@hotmail.com>
2019-09-17 18:16:48 -0400

    Configured nginx to serve static files

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -12,7 +12,7 @@ const common = require('./webpack.common');
 module.exports = merge(common, {
     mode: 'production',
     output: {
-        filename: '[name].[contentHash].bundle.js',
+        filename: 'js/[name].[contentHash].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
     optimization: {
@@ -22,7 +22,7 @@ module.exports = merge(common, {
         ]
     },
     plugins: [
-        new MiniCssExtractPlugin({filename: '[name].[contentHash].css'}),
+        new MiniCssExtractPlugin({filename: 'css/[name].[contentHash].css'}),
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: './src/template.html',
